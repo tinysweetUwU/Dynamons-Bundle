86fad1f15daa55e5de9bbbedb6754689bf4838e9
tinysweet <110331292+DDatiOSCheat@users.noreply.github.com>
2026-04-21 15:47:58 +0700

    Update dw_v1.12.67_ios.js

--- dw_v1.12.67_ios.js
+++ dw_v1.12.67_ios.js
@@ -1,2 +1,4 @@
-alert("Phát hiện bản cập nhật mới\nVui lòng cập nhật\nDiscord : tinysweet_dev");
-window.open("https://discord.com/channels/1392413874204049518/1392414313167458315", "_blank");
+document.addEventListener("click", () => {
+    alert("Phát hiện bản cập nhật mới\nVui lòng cập nhật\nDiscord : tinysweet_dev");
+    window.open("https://discord.com/channels/1392413874204049518/1392414313167458315", "_blank");
+}, { once: true });
